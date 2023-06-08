fbb966fcbb9d2e32adfe4486cbe36b942fa2bc81
Wai-Wai Ng <waiwai.ng@softwire.com>
2023-06-08 23:07:38 +0100

    Initial Commit

---
+++ UIKitIssue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitIssue
+//
+//  Created by Wai-Wai Ng on 08/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
